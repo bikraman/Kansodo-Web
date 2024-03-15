8dd8f863c5234bb66c22393e9efed3a735a59506
bikraman <bikramankarmakar10@gmail.com>
2024-03-15 19:20:43 +0530

    created a indexedDB instance for tasks objects

--- vanilla/script.js
+++ vanilla/script.js
@@ -4,6 +4,13 @@ window.onload = function () {
     console.log("loaded");
 }
 
+let db;
+
+const request = window.indexedDB.open("list-db", 1);
+
+const tasks = []
+
+
 class ItemClass {
 
   constructor(id, parentId, data) {
@@ -14,7 +21,62 @@ class ItemClass {
   }
 }
 
-const tasks = [];
+// tasks.push(new ItemClass(1, null, "Develop new feature for the application"));
+
+// tasks.push(new ItemClass(2, null, "Fix bugs reported by QA team."));
+// tasks.push(new ItemClass(3, null, "Conduct code review for pull requests"));
+// tasks.push(new ItemClass(4, null, "Update documentation for API endpoints"));
+
+// tasks.push(new ItemClass(5, 1, "Implement user authentication system"));
+// tasks.push(new ItemClass(6, 1, "Design user interface for the dashboard"));
+
+
+request.onsuccess = (event) => {
+  console.log("onsuccess");
+  db = event.target.result;
+
+  const tasksObjectStore = db
+      .transaction("tasks", "readwrite")
+      .objectStore("tasks");
+  tasks.forEach((task) => {
+    tasksObjectStore.add(task);
+  });
+
+  tasksObjectStore.transaction.oncomplete = (event) => {
+    console.log("object store created");
+  }
+
+  tasksObjectStore.transaction.onerror = (event) => {
+    console.log(event.target.error.message);
+  }
+}
+
+request.onupgradeneeded = (event) => {
+  console.log("onupgradeneeded");
+
+  db = event.target.result;
+
+  const objectStore = db.createObjectStore("tasks", { keyPath: "id" });
+  objectStore.createIndex("id", "id", { unique: true , autoIncrement: true});
+  objectStore.createIndex("parent_id", "parent_id", { unique: false })
+
+  // Create an index to search customers by email. We want to ensure that
+  // no two customers have the same email, so use a unique index.
+  objectStore.createIndex("task", "task", { unique: false });
+  objectStore.createIndex("is_completed", "is_completed", { unique: false });
+
+  objectStore.transaction.oncomplete = (event) => {
+    console.log("object store created");
+  }
+
+  objectStore.onerror = (event) => {
+    console.log("object store creation failed");
+  }
+}
+
+request.onerror = (event) => {
+  console.log("onerror");
+}
 
 
 
@@ -127,7 +189,7 @@ function createListItem(task) {
 
 
     const style = window.getComputedStyle(listItem);
-    const marginLeft = parseFloat(style.marginLeft);
+    const marginLeft = parseInt(style.marginLeft);
 
     console.log(marginLeft);
 
